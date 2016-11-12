fd274eb175050e224457743a91d3787686aaad00
FND <fnd@localhost.localdomain>
2016-11-12 09:18:30 +0100

    removed spurious exports
    it seems awkwardly redundant; if you want to get at the class, import
    the respective module

--- src/custom_tag/index.js
+++ src/custom_tag/index.js
@@ -1,8 +1,6 @@
 /* eslint-env browser */
 import MyCustomTag from "./element";
 
-export default MyCustomTag;
-
 document.registerElement("my-custom-tag", {
 	prototype: MyCustomTag.prototype
 });
--- src/type_extension/index.js
+++ src/type_extension/index.js
@@ -1,8 +1,6 @@
 /* eslint-env browser */
 import MyTypeExtension from "./element";
 
-export default MyTypeExtension;
-
 document.registerElement("my-type-extension", {
 	prototype: MyTypeExtension.prototype,
 	extends: "input"
